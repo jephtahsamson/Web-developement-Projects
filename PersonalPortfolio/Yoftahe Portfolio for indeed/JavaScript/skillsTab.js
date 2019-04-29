12051d1a7db2e146035d44f22ca24316adbab90f
Jephtah <yoftahe.samson@gmail.com>
2019-04-28 20:39:49 -0700

    Add files via upload

---
+++ PersonalPortfolio/Yoftahe Portfolio for indeed/JavaScript/skillsTab.js
@@ -0,0 +1,14 @@
+// script for tab function at skills section
+function openExpertise(evt, languageName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(languageName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
